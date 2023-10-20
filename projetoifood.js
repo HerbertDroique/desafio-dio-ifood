83d15dd261713283399e8075fc2c2813cb05b4d2
HerbertDroique <droique15@gmail.com>
2023-10-19 21:49:14 -0300

    desafio finalizado

--- projetoifood.js
+++ projetoifood.js
@@ -5,6 +5,41 @@ let nameHeroi = "Droique"
 let nivelHeroi = 2500
 
 
+if (nivelHeroi <= 1000){
+    
+    let statusHeroi = "Ferro"
+    console.log("O Herói de Nome: " +nameHeroi, "Está no nível: " +statusHeroi)
+}
+else if (nivelHeroi >= 1001 && nivelHeroi <= 2000){
+    statusHeroi = "Bronze"
+    console.log("O Herói de Nome: " +nameHeroi, "Está no nível: " +statusHeroi)
+}
+else if (nivelHeroi >= 2001 && nivelHeroi < 5000){
+    statusHeroi = "Prata"
+    console.log("O Herói de Nome: " +nameHeroi, "Está no nível: " +statusHeroi)
+}
+else if (nivelHeroi >= 6001 && nivelHeroi <= 7000){
+    statusHeroi = "Ouro"
+    console.log("O Herói de Nome: " +nameHeroi, "Está no nível: " +statusHeroi)
+}
+else if (nivelHeroi >= 7001 && nivelHeroi <= 8000){
+    statusHeroi = "Platina"
+    console.log("O Herói de Nome: " +nameHeroi, "Está no nível: " +statusHeroi)
+}
+else if (nivelHeroi >= 8001 && nivelHeroi <= 9000){
+    statusHeroi = "Ascendente"
+    console.log("O Herói de Nome: " +nameHeroi, "Está no nível: " +statusHeroi)
+}
+else if (nivelHeroi >= 9001 && nivelHeroi <= 10000){
+    statusHeroi = "Imortal"
+    console.log("O Herói de Nome: " +nameHeroi, "Está no nível: " +statusHeroi)
+}
+else if (nivelHeroi >= 10001){
+    statusHeroi = "Radiante"
+    console.log("O Herói de Nome: " +nameHeroi, "Está no nível: " +statusHeroi)
+}
+
+
 
 
 
